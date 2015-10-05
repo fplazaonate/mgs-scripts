702d8fa52de43b4dd388a5f908a6aca77067353d
Florian Plaza Oñate <florian.plaza@jouy.inra.fr>
2015-10-05 11:13:53 +0200

    Update extract_clusters_size.py

--- extract_clusters_motus.py
+++ extract_clusters_motus.py
@@ -60,7 +60,7 @@ def get_parameters():
 
     return parser.parse_args()
 
-def parse_clusters_file(clusters_file):
+def parse_clusters_file(clusters_file, min_cluster_size):
     """ Read the clusters file and creates a dict which maps a cluster to the list of its genes
     """
 
@@ -71,7 +71,8 @@ def parse_clusters_file(clusters_file):
             cluster_name, gene_name = line.split()
             cluster_to_genes[cluster_name].append(gene_name)
 
-    return cluster_to_genes
+    return dict((cluster,cluster_genes)
+            for cluster,cluster_genes in cluster_to_genes.items() if len(cluster_genes) >= min_cluster_size)
 
 def parse_motus_file(motus_file):
     """ Read the mOTUs file 
@@ -82,7 +83,8 @@ def parse_motus_file(motus_file):
 
     with open(motus_file, 'r') as istream:
         for line in istream:
-            gene_name, motu_name = line.split()
+            line_items = line.split()
+            gene_name, motu_name = line_items[0], line_items[-1]
             all_motus.add(motu_name)
             gene_to_motu[gene_name] = motu_name
     
@@ -91,6 +93,7 @@ def parse_motus_file(motus_file):
 def extract_clusters_motus(cluster_to_genes, all_motus, gene_to_motu):
     cluster_motus = dict()
     for cluster, cluster_genes in cluster_to_genes.items():
+
         cluster_motus[cluster] = dict((motu_name,[]) for motu_name in all_motus)
 
         for gene_name in cluster_genes:
@@ -99,11 +102,8 @@ def extract_clusters_motus(cluster_to_genes, all_motus, gene_to_motu):
 
     return cluster_motus
 
-def write_clusters_motus(output_dir, cluster_to_genes, cluster_motus, all_motus,  min_cluster_size):
+def write_clusters_motus(output_dir, cluster_to_genes, cluster_motus, all_motus):
     for cluster_name, cluster_genes in cluster_to_genes.items():
-
-        if len(cluster_genes) < min_cluster_size :
-            continue
 
         output_file = os.path.join(output_dir, cluster_name + '.mOTUs.txt')
 
@@ -114,13 +114,13 @@ def write_clusters_motus(output_dir, cluster_to_genes, cluster_motus, all_motus,
 def main():
     parameters = get_parameters()
     print('STEP 1/3: Reading clusters file...')
-    cluster_to_genes = parse_clusters_file(parameters.clusters_file)
+    cluster_to_genes = parse_clusters_file(parameters.clusters_file, parameters.min_cluster_size)
     print('STEP 2/4: Reading mOTUs file...')
     all_motus, gene_to_motu = parse_motus_file(parameters.motus_file)
     print('STEP 3/4: Extracting clusters mOTUs...')
     cluster_motus = extract_clusters_motus(cluster_to_genes, all_motus, gene_to_motu)
     print('STEP 4/4: Writing clusters mOTUs...')
-    write_clusters_motus(parameters.output_dir, cluster_to_genes, cluster_motus, all_motus, parameters.min_cluster_size)
+    write_clusters_motus(parameters.output_dir, cluster_to_genes, cluster_motus, all_motus)
 
 if __name__ == '__main__':
     main()
